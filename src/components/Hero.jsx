7e5c33910d264df27cf71fac0d3151e75511fc07
emrey2 <emrewp2@gmail.com>
2023-02-09 11:14:38 +0300

    -buton

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,18 +16,12 @@ const Hero = () => {
       <div className="absolute top-0 w-full h-full flex flex-col justify-center text-white">
         <div className="md:left-[10%] max-w-[1100px] m-auto absolute p-4">
           <p>Bir Ömür Boyu Maceraya Yelken Açın</p>
-          <h1 className="font-bold text-3xl md:text-3xl drop-shadow-2xl">
-            İster sevdiklerinizle samimi bir kaçamak, ister arkadaşlarınız ve
-            ailenizle unutulmaz bir tatil arıyor olun, sizin için mükemmel
-            gemimiz var.
+          <h1 className="font-bold text-4xl md:text-7xl drop-shadow-2xl">
+            AL-SAT-KİRALA
           </h1>
-          <p className="max-w-[600px] drop-shadow-2xl py-2 text-xl">
-            Her biri en son olanaklar ve teknolojilerle donatılmış geniş yat
-            seçeneklerimizle, ihtiyaçlarınıza göre tamamen özelleştirilmiş bir
-            yelken deneyiminin keyfini çıkarabilirsiniz. Nefes kesen gün
-            batımlarından nefes kesen deniz manzaralarına kadar yolculuğunuzun
-            her anı unutulmaz olacak.
-          </p>
+          <button className="bg-white text-black mt-8 py-2 px-3 hover:shadow-xl">
+            <a href="/gallery">Göz Atın</a>
+          </button>
         </div>
       </div>
     </div>
